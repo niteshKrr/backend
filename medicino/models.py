b8ed5799359a2f674f709c3035df8dd16f2c73e5
Niteshkumar <nkumar35101@gmail.com>
2021-12-11 00:15:09 +0530

    removed warning

--- medicino/models.py
+++ medicino/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Cure(models.Model):
-    id=models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
+    id=models.IntegerField(primary_key=True)
     disease=models.CharField(max_length=100)
     medicine=models.CharField(max_length=100)
-    images = models.CharField(max_length=500, auto_created=True ,default="")
+    images = models.CharField(max_length=500,)
 
 
     def __str__(self):
