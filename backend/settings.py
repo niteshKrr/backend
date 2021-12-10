94c3eee48ecfe059208ec9bc793b37a59ae8f602
vishalsharma0657 <vishalvishnu0657@gmail.com>
2021-12-10 20:11:21 +0530

    added rest framework

--- backend/settings.py
+++ backend/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'medicino'
+    'medicino',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
